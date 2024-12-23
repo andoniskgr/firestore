4eb49bc6970e71f67d0891f4c089d3e54a7881c7
andoniskgr <andoniskgr@gmail.com>
2024-12-23 22:52:44 +0200

    upd

--- js/auth.js
+++ js/auth.js
@@ -1,75 +1,105 @@
+
 window.addEventListener("DOMContentLoaded", function () {
-  console.log("DOM loaded");
+  console.log("DOM load start");
 
+  // declare navbar components
+  const loggedOutLinks = document.querySelectorAll(".logged-out");
+  const loggedInLinks = document.querySelectorAll(".logged-in");
+  const logout_btn = this.document.querySelector("#logout_link");
+
+  // declare page components
   const register_form = this.document.querySelector("#register_form");
-  register_form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    email=register_form.email.value.toLowerCase();
-    password=register_form.password.value;
-    signup(email,password);
-  });
-
   const login_form = this.document.querySelector("#login_form");
-  login_form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    email=login_form.email.value.toLowerCase();
-    password=login_form.password.value;
-    login(email,password);
+
+  // function that checks if user logged in or not
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      setUpUi(user);
+      console.log("logged in as:", user.email);
+      // window.location.replace('main_page.html');
+    } else {
+      setUpUi();
+      console.log("not login");
+    }
   });
-});
 
-auth.onAuthStateChanged((user) => {
-  const navbar_login = document.querySelector("#login_link");
-  const navbar_register = document.querySelector("#register_link");
-  const navbar_logout = document.querySelector("#logout_link");
-  if (user) {
-    console.log("logged in as:", user.email);
-    navbar_login.classList.add("d-none");
-    navbar_register.classList.add("d-none");
-    // navbar_logout.classList.remove("d-none");
-    window.location.replace('main_page.html');
-  } else {
-    navbar_login.classList.remove("d-none");
-    navbar_register.classList.remove("d-none");
-    // navbar_logout.classList.add("d-none");
-    console.log("not login");
+  // add event listeners
+  logout_btn.addEventListener("click", logout);
+  register_form.addEventListener("submit", signup);
+  login_form.addEventListener("submit", login);
+
+  // function control UI components
+  function setUpUi(user) {
+    if (user) {
+      loggedInLinks.forEach(function (link) {
+        link.classList.remove("d-none");
+      });
+      loggedOutLinks.forEach(function (link) {
+        link.classList.add("d-none");
+      });
+    } else {
+      loggedInLinks.forEach(function (link) {
+        link.classList.add("d-none");
+      });
+      loggedOutLinks.forEach(function (link) {
+        link.classList.remove("d-none");
+      });
+    }
   }
+
+  // function for controlling log out
+  function logout() {
+    let response = window.confirm("Are you sure you want to log out?");
+    if (response) {
+      auth.signOut();
+    } else {
+      return;
+    }
+  }
+
+  // function controlling signup
+  function signup(e) {
+    e.preventDefault();
+    console.log("signup function");
+    const email = register_form.email.value.toLowerCase();
+    const password = register_form.password.value;
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        console.log("Signup successfully as:", userCredential.email);
+        $(".modal").modal("hide");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log("error code:", errorCode);
+        console.log("error message:", errorMessage);
+      });
+  }
+
+  // function controlling login
+  function login(e) {
+    console.log("login function");
+    e.preventDefault();
+    const email = login_form.email.value.toLowerCase();
+    const password = login_form.password.value;
+    console.log(email, password);
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        console.log("Login successfully as:", userCredential.email);
+        $(".modal").modal("hide");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log("error code:", errorCode);
+        console.log("error message:", errorMessage);
+      });
+  }
+
+  console.log("DOM load end");
 });
 
-function signup(usr,pwd) {
-  console.log("signup function");
-  console.log(usr, pwd);
-  auth
-    .createUserWithEmailAndPassword(usr, pwd)
-    .then((userCredential) => {
-      console.log("Signup successfully as:", userCredential.email);
-      $(".modal").modal("hide");
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log("error code:", errorCode);
-      console.log("error message:", errorMessage);
-    });
-}
 
-function login(usr,pwd) {
-  console.log("login function");
-  console.log(usr, pwd);
-  auth
-    .signInWithEmailAndPassword(usr, pwd)
-    .then((userCredential) => {
-      console.log("Login successfully as:", userCredential.email);
-      $(".modal").modal("hide");
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log("error code:", errorCode);
-      console.log("error message:", errorMessage);
-    });
-}
 
-function logout() {
-  auth.signOut();
-}
