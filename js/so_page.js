92b0b1c7074c6e5be89303a1fd8defbc46dfe0b8
andoniskgr <andoniskgr@gmail.com>
2025-01-04 20:54:08 +0200

    upd

--- js/so_page.js
+++ js/so_page.js
@@ -5,41 +5,38 @@ auth.onAuthStateChanged((user) => {
 });
 
 window.addEventListener("DOMContentLoaded", function () {
- 
-    db.collection('aircrafts').orderBy("REGISTRATION").onSnapshot(function(snapshot){
-      let aircrafts=snapshot.docChanges();
-      const registration_selection=document.querySelector('#registration')
-      aircrafts.forEach(aircraft => {
-        let newOptionItem=document.createElement('option');
-        newOptionItem.text=aircraft.doc.data().REGISTRATION;
-        newOptionItem.value=aircraft.doc.data().REGISTRATION;
-        registration_selection.appendChild(newOptionItem)
-      });
-      
-      
-    })
-    
   const logout_btn = this.document.querySelector("#logout_link");
-  const type_selection_rb=this.document.querySelectorAll('[name="service_order_type"]');
+  const type_selection_rb = this.document.querySelectorAll(
+    '[name="service_order_type"]'
+  );
+  const so_form = this.document.querySelector("#service_order_form");
 
-  type_selection_rb.forEach(function (rb) {
-    rb.addEventListener("input", function (e) {
-      if (e.target.value == "pirep") {
-        console.log(e.target.value);
-        document.querySelector('#pirep_defect').classList.remove('d-none')
-        document.querySelector('#maintenance_action').classList.add('d-none')
-      } else if (e.target.value == "maint") {
-        console.log(e.target.value);
-        document.querySelector('#pirep_defect').classList.add('d-none')
-        document.querySelector('#maintenance_action').classList.remove('d-none')
-      }
+  // get aircraft data from database and build aircraft selection field
+  db.collection("aircrafts")
+    .orderBy("REGISTRATION")
+    .onSnapshot(function (snapshot) {
+      let aircrafts = snapshot.docChanges();
+      const registration_selection = document.querySelector("#registration");
+      aircrafts.forEach((aircraft) => {
+        let newOptionItem = document.createElement("option");
+        newOptionItem.text = aircraft.doc.data().REGISTRATION;
+        newOptionItem.value = aircraft.doc.data().REGISTRATION;
+        registration_selection.appendChild(newOptionItem);
+      });
     });
-  });
-  
 
+    // add event listeners
+    type_selection_rb.forEach(function (rb) {
+      rb.addEventListener("input", setupFormUi);
+    });
 
-  logout_btn.addEventListener("click", logout);
+    logout_btn.addEventListener("click", logout);
+    so_form.addEventListener('submit', handleForm)
 });
+  
+  
+  
+  
 
 function logout() {
   let response = window.confirm("Are you sure you want to log out?");
@@ -67,4 +64,44 @@ function formatTime(event) {
 
   // Update the input value with the formatted time
   input.value = value;
+}
+
+function setupFormUi(e) {
+  if (e.target.value == "pirep") {
+    document.querySelector("#pirep_defect").classList.remove("d-none");
+    document.querySelector("#maintenance_action").classList.add("d-none");
+  } else if (e.target.value == "maint") {
+    document.querySelector("#pirep_defect").classList.add("d-none");
+    document.querySelector("#maintenance_action").classList.remove("d-none");
+  }
+}
+
+function handleForm(e){
+  e.preventDefault();
+  const flight=e.target.flight.value;
+  const from=e.target.from.value;
+  const to=e.target.to.value;
+  const eta=e.target.eta.value;
+  const registration=e.target.registration.value;
+  const mel_desc=e.target.mel_description.value;
+  const mel=e.target.mel.value;
+  const final=e.target.so_prepared_test;
+  let res=`A/C DETAILS:
+${registration} (Aircraft Type: A321-231, MSN: 02610 ENG TYPE: V2500), FLT No ${flight} (${from}-${to}), ETA:04/01/2025 ${eta} UTC.
+
+DEFECT DETAILS:
+A/c released iaw ${mel} ( ${mel_desc}). Please perform the necessary maintenance action iaw maintenance procedures section 28-40-00-040-005-A. before a/c departure.
+
+NOTE:
+By receiving the attached training material and along with the CRS, you confirm knowledge of the operator’s processes and procedures.
+
+INFO:
+Pls raise a new workorder in the Tech Log in order to record any maintenance action and close the workorder.
+Upon completion, please leave the white original workorder page in the Tech log and fax the green workorder copy back to Athens MCC before a/c departure.
+
+AIRCRAFT MANUALS
+Access to manuals is made by AirnavX using the link : https://extranet.aegeanair.com Username and password are provided by MCC.
+`;
+  final.value+=res;
+  
 }
