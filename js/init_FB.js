e2f7777e4dd215aaac7a72f6f1a80495c2f1347d
andoniskgr <andoniskgr@gmail.com>
2025-01-09 13:11:38 +0200

    up

--- js/init_FB.js
+++ js/init_FB.js
@@ -20,6 +20,6 @@ const db = firebase.firestore();
 db.settings({ timestampInSnapshots: true });
 
 
-const events_collection="events1"
+const events_collection="events"
 const aircrafts_collection="aircrafts"
 
